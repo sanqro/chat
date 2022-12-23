e3ee32c08a85cc95af7fa4af089c36c9d2a5acde
mnaray <m.naray.inf21@stud.bbbaden.ch>
2022-12-23 11:09:57 +0100

    final touches eslint setup

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,9 @@
 /* eslint-disable no-undef */
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {}
   },
-  plugins: [],
+  plugins: []
 };
