86ea844ae57e8c2fd5c2c47e685c2ea49fe83b84
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-01-12 15:26:09 +0100

    compile to deployable build

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,9 +4,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 exports.__esModule = true;
 var express_1 = __importDefault(require("express"));
+var cors_1 = __importDefault(require("cors"));
+var test_1 = __importDefault(require("./routes/test"));
 var app = (0, express_1["default"])();
+app.use(express_1["default"].json());
+app.use((0, cors_1["default"])());
+app.disable("etag");
+app.use("/test", test_1["default"]);
 app.get("/", function (req, res) {
-    return res.send("This is the api for following application on GitHub: https://github.com/sanqro/chat");
+    return res.status(200).json({
+        msg: "This is the API for following application on GitHub: https://github.com/sanqro/chat"
+    });
 });
 if (!process.env.DETA_RUNTIME) {
     app.listen(3001, function () { return console.log("Started on http://localhost:3001"); });
