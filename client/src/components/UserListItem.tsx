61bbd7aea8f62c1fbff1c40fa2f4b4cea04e856a
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-01-20 16:08:27 +0100

    add hover animations to items on result list

--- client/src/components/UserListItem.tsx
+++ client/src/components/UserListItem.tsx
@@ -4,7 +4,10 @@ import OpenChatroomButton from "./OpenChatroomButton";
 
 function UserListItem(props: ISearchResult) {
   return (
-    <div className="flex flex-row justify-between border-2 border-black px-5 py-2.5 items-center">
+    <div
+      className="flex flex-row justify-between border-2 border-black px-5 py-2.5 items-center
+                  hover:scale-105 hover:border-cyan-400 transition-all"
+    >
       <p className="font-bold mr-7">{props.username}</p>
       <OpenChatroomButton username={props.username} />
     </div>
