85408f43be04097e56a58e951993c5bc2b9f1017
sandro <56117612+sanqro@users.noreply.github.com>
2023-01-22 15:25:30 +0100

    Add working registration

--- client/src/components/RegistrationField.tsx
+++ client/src/components/RegistrationField.tsx
@@ -1,17 +1,54 @@
-import React, { FormEvent } from "react";
+/* eslint-disable prefer-const */
+/* eslint-disable no-console */
+import React, { FormEvent, useState } from "react";
 import CopyBox from "./CopyBox";
 import FormInputField from "./FormInputField";
 import SubmitButton from "./SubmitButton";
 
 function RegistrationField() {
   let username: string;
+  let [privateKey, setPrivateKey] = useState("");
+  let [publicKey, setPublicKey] = useState("");
 
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+  const submitHandler = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     username = (document.getElementById("username") as HTMLInputElement).value;
     if (!verifyInput()) return;
-    alert("submit function gets called!");
-    // submit
+    await getKeys();
+    console.log("ho");
+    register();
+  };
+
+  const getKeys = async () => {
+    const response = await fetch("https://chatapp.deta.dev/keys/generateKeypair", {
+      method: "GET"
+    });
+    const responseJson = await response.json();
+    setPrivateKey((privateKey = responseJson.privateKey));
+    setPublicKey((publicKey = responseJson.publicKey));
+  };
+
+  const register = async () => {
+    console.log(publicKey);
+    const registerResponse = await fetch("https://chatapp.deta.dev/auth/register", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        username,
+        privateKey,
+        publicKey
+      })
+    });
+    const registerJson = await registerResponse.json();
+    console.log(username, privateKey, publicKey);
+    console.log(privateKey, publicKey);
+    if (registerJson.success) {
+      alert("Registration Successful!");
+    } else {
+      alert("Registration Failed: " + registerJson.error);
+    }
   };
 
   const verifyInput = () => {
@@ -32,10 +69,7 @@ function RegistrationField() {
           <FormInputField type="text" placeholder="Username" />
         </label>
         <label htmlFor="privateKey">
-          <CopyBox placeholder="New Private Key">
-            Text to Copy!Text to Copy!Text to Copy!Text to Copy!Text to Copy!Text to Copy!Text to
-            Copy!Text to Copy!Text to Copy!Text to Copy!Text to Copy!Text to Copy!Text to Copy!
-          </CopyBox>
+          <CopyBox placeholder="New Private Key">{privateKey}</CopyBox>
         </label>
         <SubmitButton>Generate Keys</SubmitButton>
       </form>
