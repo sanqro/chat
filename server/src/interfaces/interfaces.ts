2ee5886722f6d40ef2c2d7338bf29412ce8248be
sandro <56117612+sanqro@users.noreply.github.com>
2023-01-23 17:57:10 +0100

    Add interfaces for creating chatrooms

--- server/src/interfaces/interfaces.ts
+++ server/src/interfaces/interfaces.ts
@@ -15,6 +15,21 @@ export interface IUsername {
   username: string;
   publicKey?: string;
 }
+export interface IEncryptedMessage {
+  msg: string;
+  author: string;
+  dateTime: Date;
+}
+
+export interface IParticipant {
+  username: string;
+  publicKey: string;
+}
+
+export interface IChatroomData {
+  participantArray?: IParticipant[];
+  msgArray?: IEncryptedMessage[];
+}
 
 export interface IJWTPayload extends JwtPayload {
   username: string;
