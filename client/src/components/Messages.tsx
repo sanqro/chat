023e55d4abaea3611c3dd96b765867eddb3d6f9b
sandro <56117612+sanqro@users.noreply.github.com>
2023-01-25 22:59:24 +0100

    add fetchMessage

--- client/src/components/Messages.tsx
+++ client/src/components/Messages.tsx
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { IEncryptedMessage } from "../interfaces/api-req";
+import MessageBox from "./MessageBox";
 
-function Messages() {
-  return <div className="grow bg-red-500">Messages</div>;
-}
+const Messages = () => {
+  const [messages, setMessages] = useState<IEncryptedMessage[]>([]);
+
+  const authToken = sessionStorage.getItem("chatapp_token") as string;
+  const currentChat = sessionStorage.getItem("current_chat") as string;
+  let msgBoxList;
+
+  const fetchMessages = async () => {
+    if (currentChat !== null) {
+      const response = await fetch("https:///chatapp.deta.dev/chatroom/getMessages", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          Authorization: authToken
+        },
+        body: JSON.stringify({
+          currentChat
+        })
+      });
+
+      try {
+        const responseJson: IEncryptedMessage[] = await response.json();
+        setMessages(responseJson);
+      } catch (error) {
+        error instanceof Error
+          ? console.error(error.message)
+          : console.error("Unknown error occurred");
+      }
+    } else {
+      // create chatroom
+    }
+
+    msgBoxList = messages.map((messageObj) => {
+      return (
+        // eslint-disable-next-line react/jsx-key
+        <MessageBox
+          msg={messageObj.msg}
+          author={messageObj.author}
+          dateTime={messageObj.dateTime}
+        />
+      );
+    });
+  };
+
+  useEffect(() => {
+    // eslint-disable-next-line no-constant-condition
+    while (true) {
+      fetchMessages();
+    }
+  }, []);
+
+  return <div className="grow bg-blue-500">{msgBoxList}</div>;
+};
 
 export default Messages;
