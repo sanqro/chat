4ae661f8b71d6d55c509de8c0e8b9c9fa3cc6fdc
mnaray <m.naray.inf21@stud.bbbaden.ch>
2023-01-27 09:07:14 +0100

    return false instead of void on checkUser

--- server/dist/middleware/checkUser.js
+++ server/dist/middleware/checkUser.js
@@ -85,7 +85,7 @@ function checkUser(req, res, next) {
                     chatroom = _a.sent();
                     if (chatroom === null) {
                         res.status(204).json({ message: "This chatroom does not exist yet.", success: false });
-                        return [2];
+                        return [2, false];
                     }
                     if (chatroom.participantArray[0].username != username &&
                         chatroom.participantArray[1].username != username) {
